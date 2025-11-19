b39bc09525550962843d209ccac71c1a8e35c51e
shgereadis-cpu <shgereadis@gmail.com>
2025-11-19 23:22:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ async def post_init(application: ApplicationBuilder) -> None:
         await application.bot.set_webhook(url=url)
 
 # Gunicorn የሚጠብቀው ይህንን ነው (WSGI callable)
-def main_run(environ, start_response):
+def main_run(environ):
     """Gunicorn የሚጠይቃቸውን መለኪያዎች ተቀብሎ የቴሌግራም አፕሊኬሽኑን ያስነሳል።"""
     
     init_db()
